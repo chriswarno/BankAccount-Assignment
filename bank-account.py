c481c5b11d20a89d1d030ed652a182fb1cb79211
Chris Warno <cwarno95@gmail.com>
2024-11-23 17:49:58 -0500

    Added print functions to main function

--- bank-account.py
+++ bank-account.py
@@ -3,26 +3,39 @@ class BankAccount:
     balance = float
     first_name = str
     last_name = str
+
     def __init__(self, id, first_name, last_name):
         self.id = id
         self.first_name = first_name
         self.last_name = last_name
 
+
     def withdraw(self, amount):
         self.amount = amount
 
+
     def deposit(self, amount):
         self.amount = amount
+
 
     def transfer_from(self, amount, to_account):
         self.amount = amount
         self.to_account = to_account
 
+
     def __str__(self):
-        pass
+        return f"{self.id}: {self.first_name} {self.last_name}, Balance: {self.balance}"
 
 
 def main():
-    j_smith = BankAccount(1, 'John', 'Smith')
+    j_smith = BankAccount(1,'John', 'Smith')
     h_rollins = BankAccount(2, 'Harry', 'Rollins')
     l_sampson = BankAccount(3, 'Lucy', 'Sampson')
+
+    j_smith.deposit(100)
+
+    print(j_smith)
+    print(h_rollins)
+    print(l_sampson)
+
+main()
