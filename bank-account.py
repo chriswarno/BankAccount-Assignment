54beea24cc0e7fd54337de27297521f3eb4a9ca5
Chris Warno <cwarno95@gmail.com>
2024-11-23 20:07:22 -0500

    Added deposit module

--- bank-account.py
+++ bank-account.py
@@ -4,8 +4,9 @@ class BankAccount:
     first_name = str
     last_name = str
 
-    def __init__(self, id, first_name, last_name):
+    def __init__(self, id, balance, first_name, last_name):
         self.id = id
+        self.balance = balance
         self.first_name = first_name
         self.last_name = last_name
 
@@ -15,7 +16,7 @@ class BankAccount:
 
 
     def deposit(self, amount):
-        self.amount = amount
+        self.balance += amount
 
 
     def transfer_from(self, amount, to_account):
@@ -28,11 +29,18 @@ class BankAccount:
 
 
 def main():
-    j_smith = BankAccount(1,'John', 'Smith')
-    h_rollins = BankAccount(2, 'Harry', 'Rollins')
-    l_sampson = BankAccount(3, 'Lucy', 'Sampson')
+    j_smith = BankAccount(1, 0, 'John', 'Smith')
+    h_rollins = BankAccount(2, 0, 'Harry', 'Rollins')
+    l_sampson = BankAccount(3, 0, 'Lucy', 'Sampson')
+
+    print(j_smith)
+    print(h_rollins)
+    print(l_sampson)
+    print("\n")
 
     j_smith.deposit(100)
+    h_rollins.deposit(100)
+    l_sampson.deposit(100)
 
     print(j_smith)
     print(h_rollins)
