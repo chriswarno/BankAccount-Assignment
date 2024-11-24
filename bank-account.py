feed69a8cfdee5b18ec40f7defba218f5ab5a946
Chris Warno <cwarno95@gmail.com>
2024-11-23 21:07:23 -0500

    add transfer_from module

--- bank-account.py
+++ bank-account.py
@@ -26,7 +26,10 @@ class BankAccount:
 
     #module to transfer money to an account
     def transfer_from(self, amount, to_account):
-        pass
+        if self.balance < amount:
+            raise ValueError("There are not enough funds in the account to transfer $" + str(amount))
+        self.withdraw(amount)
+        to_account.deposit(amount)
 
 
     #__str__ module to print the object
@@ -49,29 +52,43 @@ def main():
     j_smith.deposit(100)
     h_rollins.deposit(100)
     l_sampson.deposit(100)
-    print("\n")
 
     print(j_smith)
     print(h_rollins)
     print(l_sampson)
     print("\n")
 
-    #withdrawing money from the accounts
+    #withdrawing money from an account which has enough money
     try:
         j_smith.withdraw(50)
     except ValueError as e:
         print(e)
         print("\n")
 
+    #withdrawing money from an account which doesn't have enough money
     try:
         h_rollins.withdraw(200)
     except ValueError as e:
         print(e)
         print("\n")
 
+    #transfering money and the from account has enough money
+    try:
+        l_sampson.transfer_from(20, j_smith)
+    except ValueError as e:
+        print(e)
+        print("\n")
+
+    #transfering money and the from account doesn't have enough money
+    try:
+        l_sampson.transfer_from(200, h_rollins)
+    except ValueError as e:
+        print(e)
+        print("\n")
+
+    #final print
     print(j_smith)
     print(h_rollins)
     print(l_sampson)
-    print("\n")
 
 main()
